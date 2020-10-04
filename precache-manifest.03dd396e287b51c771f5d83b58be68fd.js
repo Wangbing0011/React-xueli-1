bed91e197b0496535f5bfa44332066a71d8de8b4
王冰 <45160595+Wangbing0011@users.noreply.github.com>
2020-10-04 17:28:58 +0800

    React雪梨作业1-第一次提交

---
+++ precache-manifest.03dd396e287b51c771f5d83b58be68fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34dcdc5a271b20f61aa35804b0a37e23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee2008ef83b7dd1b6557",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "20f6682100a0e2a2e042",
+    "url": "/static/js/2.1c9c32f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1c9c32f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee2008ef83b7dd1b6557",
+    "url": "/static/js/main.ec8a0f59.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
